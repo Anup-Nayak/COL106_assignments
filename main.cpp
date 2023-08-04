65224c8cd03f81e26f848346c27344baaa5a5992
Anup <gpnayak1930@gmail.com>
2023-08-04 21:31:24 +0530

    ideation

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class SET{
+    public:
+    int n;
+    vector<int> v;
+
+    SET(int setNumber,vector<int> nullVector={}){
+        cout<<"constructor called and set "<<setNumber<<" is created."<<endl;
+    }
+};
+
+vector<SET> database ={};
+
+
+
+
+
+
+int main(){
+
+    SET a(1);
+};
